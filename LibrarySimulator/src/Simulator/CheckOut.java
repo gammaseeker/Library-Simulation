671cfba9660309fec28fbb751cdb77794f0db02a
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-05-25 01:39:43 -0400

    2am ninja edits

---
+++ LibrarySimulator/src/Simulator/CheckOut.java
@@ -0,0 +1,15 @@
+package Simulator;
+
+public class CheckOut implements Runnable{
+
+	public CheckOut()
+	{
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+}
--- LibrarySimulator/src/Simulator/Simulator.java
+++ LibrarySimulator/src/Simulator/Simulator.java
@@ -1,5 +1,7 @@
 package Simulator;
 
+import Objects.Person;
+
 public class Simulator implements Runnable{
 	
 	public Simulator()
@@ -9,7 +11,23 @@ public class Simulator implements Runnable{
 
 	@Override
 	public void run() {
-		System.out.println("Test");
+		System.out.println(createPerson());
+		System.out.println();
+		System.out.println(createPerson());
+		System.out.println();
+		System.out.println(createPerson());
 		
+	}
+	
+	private Person createPerson()
+	{
+		String[] first = {"John", "Steve", "Bob", "James"};
+		String[] last = {"Doe", "Smith", "Jenson", "Gates"};
+		return new Person(first[getRandomIndex(0, first.length)], last[getRandomIndex(0, last.length)], 1);
+	}
+	
+	private int getRandomIndex(int lower, int upper)
+	{
+		return (int)(Math.random() * (upper - lower)) + lower;
 	}
 }
