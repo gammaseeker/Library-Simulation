44e15d28ddc6f204cbd1bc71e1e1c7dd26d92bf7
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-08-10 09:14:29 -0400

    Update Run.java

--- LibrarySimulator/src/Simulator/Run.java
+++ LibrarySimulator/src/Simulator/Run.java
@@ -3,9 +3,9 @@ package Simulator;
 public class Run {
 	public static void main(String[] args)
 	{
-	/*	Runnable sim1 = new Simulator();
+		Runnable sim1 = new Simulator();
 		Thread simulation1 = new Thread(sim1);
-		simulation1.start();*/
+		simulation1.start();
 		
 		Runnable timer = new Counter(5000, 10);
 		Thread timer1 = new Thread(timer);
