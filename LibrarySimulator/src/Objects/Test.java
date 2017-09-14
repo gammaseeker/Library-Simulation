1320beebd23c7b1c14a73f2680e91548da5dd742
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-09-14 16:18:30 -0400

    Wrote Person tests

--- LibrarySimulator/src/Objects/Test.java
+++ LibrarySimulator/src/Objects/Test.java
@@ -14,7 +14,7 @@ public class Test {
 		Book b2 = new Book("Julius Caesar", "Shakespeare", 1);
 		
 		
-		System.out.println(b2);// toSTring();
+		System.out.println(b2);// toString();
 		System.out.println("Title: " + b2.getTitle());
 		System.out.println("Author: " + b2.getAuthor());
 		System.out.println("ID: " + b2.getId());
@@ -30,5 +30,25 @@ public class Test {
 		System.out.println("Borrowed: " + b1.isBorrowed());
 		b1.borrow();
 		System.out.println("Borrowed: " + b1.isBorrowed());
+	}
+	
+	private static void testPerson(){
+		Person p1 = new Person("John", "Doe", 1);
+		
+		Stack<Book> st = new Stack<Book>();
+		st.push(new Book("Brave New World", "Aldous Huxely"));
+		st.push(new Book("Julius Caesar", "Shakespeare", 1));
+		
+		double fees = 10.0;
+		
+		Person p2 = new Person("Foo", "Bar", 2, st, fees);
+		
+		System.out.println(p1);// toString();
+		System.out.println("First Name: " + p1.getFirstName());
+		System.out.println("Last Name: " + p1.getLastName());
+		System.out.println("Books: " + p1.getBooks());
+		System.out.println("ID: " + p1.getLibraryId());
+		System.out.println("Fees:" + p1.getFees());
+		System.out.println(p2);// toString();
 	}
 }
